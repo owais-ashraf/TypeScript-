b97b704133554c67bd8e8d77ea6458d9fccfa311
owais-ashraf <owais.ashraf72@gmail.com>
2024-07-20 15:38:58 +0500

    chore : amend index.ts

--- Modules/4. index.ts
+++ Modules/4. index.ts
@@ -2,12 +2,17 @@
 
 import { getAllStudents , isEntryTestPassed, isOnSite, findStudents, isOnSiteAllowed} from "./3. allFunctions";
 
-// console.log(getAllStudents()); // log all students list
+console.log(getAllStudents()); // log all students list
+console.log("##########################################################################");
 
-// console.log(isEntryTestPassed());  // log all passed students list
+console.log(isEntryTestPassed());  // log all passed students list
+console.log("##########################################################################");
 
-// console.log(isOnSite());         // log all on site allowed students list
+console.log(isOnSite());         // log all on site allowed students list
+console.log("##########################################################################");
 
-// console.log(findStudents(2005));     // log student by Roll num 
+console.log(findStudents(2005));     // log student by Roll num 
+console.log("##########################################################################");
 
 console.log(isOnSiteAllowed(2004));     // check student roll num and also check if he is allowed on site or not 
+console.log("##########################################################################");
